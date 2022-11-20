0b1313a783fca06054c5aa0e2e9eebced8a1ddcd
Harshal Raikwar <harshalraikwar07@gmail.com>
2022-11-20 17:49:36 +0530

    ✨ Add Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+import HeroImg from "../assets/hero.svg";
+import FeatImg from "../assets/feat.svg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="md:mx-28 mx-4 text-white py-10">
+      <div className="md:grid md:grid-cols-2 items-center">
+        <div className="">
+          <h1 className="text-3xl md:text-6xl">Who we are</h1>
+          <p className="text-xl py-4 tracking-wider text-justify">About the web app</p>
+
+          <Link to="/lungCancerPrediction">
+            <button className="bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl">
+              Lung Cancer Prediction
+            </button>
+          </Link>
+        </div>
+        <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
+          <img src={HeroImg} alt="img" width="350" height="350" />
+        </div>
+      </div>
+
+      <div className="md:grid md:grid-cols-2 pt-12 items-center">
+        <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
+          <img src={FeatImg} alt="img" width="350" height="350" />
+        </div>
+        <div className="">
+          <h1 className="text-3xl md:text-5xl">What else do we have</h1>
+          <p className="text-xl md:text-2xl py-4 tracking-wider">
+            APP NAME is a web app where you can:
+          </p>
+          <ul className="text-xl">
+            <li className="list-disc">Easy to use.</li>
+            <li className="list-disc">Predict lung cancer</li>
+            <li className="list-disc">Upload the image Chest CT Scan image.</li>
+            <li className="list-disc">A web app where you can check wether you have Lung Cancer or Not.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
