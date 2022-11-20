290d5a76a573c444bc797f8d32e0b17900438a8a
Harshal Raikwar <harshalraikwar07@gmail.com>
2022-11-20 16:58:02 +0530

    ✨ Add navbar

--- client/src/container/Main.js
+++ client/src/container/Main.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
 
 function Main() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default Main;
